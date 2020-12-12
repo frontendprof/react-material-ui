131db53b4e0e38d63fbcf20cbfae0207c05ca013
frontendprof <https://github.com/frontendprof>
2020-12-12 18:37:16 +0500

    Fixing navbar section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
+import { Container,Toolbar,AppBar,IconButton,Typography,Box } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/icons/Menu';
+import {makeStyles} from "@material-ui/core/styles";
 
-import './App.css';
+
+
+
+const useStyles=makeStyles((theme)=>({
+  root:{
+    flexGrow:1
+  },
+  menuButton:{
+    marginRight:theme.spacing(3)
+  },
+  title:{
+    flexGrow:1
+  }
+}))
+
+
 
 function App() {
+
+  const classes=useStyles();
+
+
   return (
     <div className="App">
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar>
 
-      <Button variant="contained">Default</Button>
-      <Button variant="contained" color="primary">
-        Primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        Secondary
-      </Button>
-      <Button variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button variant="contained" color="primary" href="#contained-buttons">
-        Link
-      </Button>
+            <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}>
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" className={classes.title}>AbdulMalik's Website</Typography>
+
+            <Box mr={3}>
+              <Button color="inherit" variant="outlined">Log in</Button>
+            </Box>
+            <Button color="secondary" variant="contained">Sign Up</Button>
+
+          </Toolbar>
+        </Container>
+      </AppBar>
      
     </div>
   );
