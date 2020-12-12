2d943923a081cbce24b6521503e9c9ba9e751e50
frontendprof <https://github.com/frontendprof>
2020-12-12 22:16:12 +0500

    Grid system application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container,Toolbar,AppBar,IconButton,Typography,Box } from '@material-ui/core';
+import { Container,Toolbar,AppBar,IconButton,Typography,Box,Paper,Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import MenuIcon from '@material-ui/icons/Menu';
 import {makeStyles} from "@material-ui/core/styles";
@@ -16,6 +16,28 @@ const useStyles=makeStyles((theme)=>({
   },
   title:{
     flexGrow:1
+  },
+  mainFeaturesPost:{
+    position:"relative",
+    color:theme.palette.common.white,
+    marginBottom:theme.spacing(4),
+    backgroundSize:"cover",
+    backgroundRepeat:"no-repeat",
+    backgroundPosition:"center"
+  },
+  overlay:{
+    position:"absolute",
+    top:0,
+    right:0,
+    bottom:0,
+    left:0,
+    backgroundOverlay:"rgba(0,0,0,.3)"
+
+  },
+  mainFeaturesPostContent:{
+    position:"relative",
+    padding:theme.spacing(9)
+
   }
 }))
 
@@ -27,7 +49,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <>
       <AppBar position="fixed">
         <Container fixed>
           <Toolbar>
@@ -46,8 +68,43 @@ function App() {
           </Toolbar>
         </Container>
       </AppBar>
+
+      <main>
+        <Paper className={classes.mainFeaturesPost} style={{ backgroundImage:`url(https://source.unsplash.com/random)` }}>
+          <Container fixed>
+            <div className={classes.overlay} />
+            <Grid container>
+              <Grid item md={6}>
+                <div className={classes.mainFeaturesPostContent}>
+
+                  <Typography
+                    component="h1"
+                    variant="h3"
+                    color="inherit"
+                    gutterBottom
+                  >
+                    Welcome to my awesome blog
+                  </Typography>
+
+                  <Typography
+                    component="h5"
+                    color="inherit"
+                    paragraph
+                  >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero quidem sequi, magnam suscipit rerum.
+                  </Typography>
+
+                  <Button varian="contained" color="secondary">
+                    Learn More
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
+        </Paper>
+      </main>
      
-    </div>
+    </>
   );
 }
 
